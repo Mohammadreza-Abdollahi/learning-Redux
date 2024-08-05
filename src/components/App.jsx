d47d57538f6c7dde459b95af6cbfa6cfd0181735
Mohammadreza Abdollahi <mohammad1384abdollahi@gmail.com>
2024-08-05 23:23:32 +0330

    version 0.0.1
    create my react app and add bootstrap style and...

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return ( 
+        <>
+            <div className="text-center">
+                <h1 className="btn btn-success my-1">My App</h1>
+            </div>
+        </>
+    );
+}
+ 
+export default App;
